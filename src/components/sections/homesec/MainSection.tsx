9b6b5ff99dcddc37f0a0d6e2dda8585694499a4e
Adilet25 <baryktabasovadi@gmail.com>
2024-09-17 00:00:07 +0600

    changed mainsection background

--- src/components/sections/homesec/MainSection.tsx
+++ src/components/sections/homesec/MainSection.tsx
@@ -1,11 +1,38 @@
 import { useNavigate } from "react-router-dom";
 import "../../../styles/MainSection.css";
-
+import { useEffect, useState } from "react";
+type TableRowData = string[];
 const MainSection = () => {
   const navigate = useNavigate();
+  const [widthW, setWidth] = useState(window.innerWidth);
+
+  //! The TD's amount
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      setCellsPerRow(getCellsPerRow(window.innerWidth));
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [widthW]);
+  const getCellsPerRow = (width: number) => {
+    return widthW / 70; //? The amount x
+  };
+  const [cellsPerRow, setCellsPerRow] = useState<number>(
+    getCellsPerRow(window.innerWidth)
+  );
+  const generateTableData = (numCells: number) => {
+    const data: TableRowData[] = [];
+    for (let i = 0; i < 10; i++) {
+      //? The 10 is amount y
+      data.push(Array.from({ length: numCells }, (_, index) => ``));
+    }
+    return data;
+  };
+  const tableData = generateTableData(cellsPerRow);
   return (
-    <div className="mainSec">
-      <div className="containers">
+    <div>
+      <div className="mainSec">
         <div className="mainSec_info">
           <p>
             <span id="green">Matrix</span> - играй умом
@@ -16,6 +43,21 @@ const MainSection = () => {
           >
             Играть
           </button>
+        </div>
+        <div className="tables">
+          <table>
+            <tr>
+              {tableData.map((rowData, rowIndex) => (
+                <tr key={rowIndex}>
+                  {rowData.map((cellData, cellIndex) => (
+                    <td key={cellIndex} className="tableTd">
+                      {cellData}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tr>
+          </table>
         </div>
       </div>
     </div>
