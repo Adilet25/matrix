995fbe05925e063c6e8d29c0416e952c89b87ebe
Adilet25 <baryktabasovadi@gmail.com>
2026-03-12 03:43:53 +0600

    testing login facei2

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,7 +1,7 @@
 
 const LoginPage = () => {
   return (
-    <div><a href="https://matrix-8of6.onrender.com/auth/faceit/login">FACEIT LOGIN</a></div>
+    <div><a href="https://matrix-8of6.onrender.com/auth/faceit/login" className="bg-white mt-1rem">FACEIT LOGIN</a></div>
   )
 }
 
